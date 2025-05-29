ed1a597b5fe1889e4ec6593e9877cefffca194a3
AndresPescador <94087468+AndresPescador@users.noreply.github.com>
2025-05-29 16:18:46 -0500

    v1

---
+++ src/test/java/com/example/TallerSpringApi/TallerSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TallerSpringApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
